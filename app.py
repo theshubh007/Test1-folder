545a984e6257aa189b194f56a677aca204d021c6
shubham <thekingshubh07@gmail.com>
2023-09-29 14:42:05 +0530

    v

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
-
-from flask import Flask, request, jsonify, render_template,json,current_app
-from flask_restful import Resource, Api
-import urllib.request
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
-import urllib.parse
-
-
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return jsonify({'message':'Hello World!'})
-
-
-
-
-
-
-
+    return "Hello World!"
 
     
 # if __name__ == '__main__':
