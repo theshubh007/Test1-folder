e8c951c4765d569da90dfdf9d50faf642c07714d
shubham <thekingshubh07@gmail.com>
2023-09-29 14:44:33 +0530

    v1

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-    return "Hello World!"
+    return jsonify({'message':'Hello World!'})
 
     
 # if __name__ == '__main__':
